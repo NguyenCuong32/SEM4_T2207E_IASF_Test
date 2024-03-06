6169699b4e47be5e8b22ffa5925c2b2811008800
nguyễn trung hiếu <youngspidey2301@gmail.com>
2024-03-06 19:47:58 +0700

    first commit

--- prac/src/main/java/com/example/prac/controller/Vcontroller.java
+++ prac/src/main/java/com/example/prac/controller/Vcontroller.java
@@ -1,5 +1,15 @@
 package com.example.prac.controller;
 
-public class Vcontroller {
+import com.example.prac.service.IService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/")
+public class Vcontroller {
+    private final IService iService;
+
+    public Vcontroller(IService iService) {
+        this.iService = iService;
+    }
 }
