fa68f5b40d84d72890c0548c57a12934ea2dcf1f
nguyễn trung hiếu <youngspidey2301@gmail.com>
2024-03-06 19:49:59 +0700

    first commit

--- prac/src/main/java/com/example/prac/controller/Vcontroller.java
+++ prac/src/main/java/com/example/prac/controller/Vcontroller.java
@@ -1,7 +1,10 @@
 package com.example.prac.controller;
 
 import com.example.prac.service.IService;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,4 +15,10 @@ public class Vcontroller {
     public Vcontroller(IService iService) {
         this.iService = iService;
     }
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        dataBinder.registerCustomEditor(String.class,stringTrimmerEditor);
+    }
+
 }
