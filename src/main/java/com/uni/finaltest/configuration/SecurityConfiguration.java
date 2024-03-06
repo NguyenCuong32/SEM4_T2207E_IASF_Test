091051f66c92aef05b019c90c9ec78e4404ca332
pokabi11 <hlong2k1@gmail.com>
2024-03-06 19:53:42 +0700

    Security login logout

--- src/main/java/com/uni/finaltest/configuration/SecurityConfiguration.java
+++ src/main/java/com/uni/finaltest/configuration/SecurityConfiguration.java
@@ -2,8 +2,11 @@ package com.uni.finaltest.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
 
@@ -15,5 +18,23 @@ public class SecurityConfiguration {
         userDetailsManager.setUsersByUsernameQuery("select user_id,password,is_active from members where user_id=?");
         userDetailsManager.setAuthoritiesByUsernameQuery("select user_id,role from roles where user_id=?");
         return userDetailsManager;
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity.authorizeHttpRequests(configSecurity->
+        {
+            configSecurity
+                    .requestMatchers("catalogue/**")
+                    .hasRole("ADMIN")
+                    .anyRequest()
+                    .authenticated();
+        }).formLogin(
+                Customizer.withDefaults()
+
+        ).exceptionHandling(
+                configHandling->configHandling.accessDeniedPage("/exception/access-denied")
+        )
+        ;
+        return httpSecurity.build();
     }
 }
