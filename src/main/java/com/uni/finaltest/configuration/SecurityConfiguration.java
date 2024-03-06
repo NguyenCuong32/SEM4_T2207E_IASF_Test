ca425250bcc5dd06febe99248cf5772b3a69e339
pokabi11 <hlong2k1@gmail.com>
2024-03-06 19:49:36 +0700

    Security login logout

---
+++ src/main/java/com/uni/finaltest/configuration/SecurityConfiguration.java
@@ -0,0 +1,19 @@
+package com.uni.finaltest.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SecurityConfiguration {
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
+        userDetailsManager.setUsersByUsernameQuery("select user_id,password,is_active from members where user_id=?");
+        userDetailsManager.setAuthoritiesByUsernameQuery("select user_id,role from roles where user_id=?");
+        return userDetailsManager;
+    }
+}
--- src/main/java/com/uni/finaltest/controllers/MonitorController.java
+++ src/main/java/com/uni/finaltest/controllers/MonitorController.java
@@ -3,6 +3,11 @@ package com.uni.finaltest.controllers;
 import com.uni.finaltest.entity.Vehicle;
 import com.uni.finaltest.service.VehicleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.logout.DelegatingServerLogoutHandler;
+import org.springframework.security.web.server.authentication.logout.SecurityContextServerLogoutHandler;
+import org.springframework.security.web.server.authentication.logout.WebSessionServerLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -39,5 +44,14 @@ public class MonitorController {
     public String DeleteVehicle(@RequestParam("vehicleId") Integer id, Model model) {
         vehicleService.deleteVehicle(id);
         return "redirect:/list";
+    }
+    public SecurityWebFilterChain http(ServerHttpSecurity http) throws Exception {
+        DelegatingServerLogoutHandler logoutHandler = new DelegatingServerLogoutHandler(
+                new WebSessionServerLogoutHandler(), new SecurityContextServerLogoutHandler()
+        );
+        http
+                .authorizeExchange((exchange)-> exchange.anyExchange().authenticated())
+                .logout((logout)->logout.logoutHandler(logoutHandler));
+        return http.build();
     }
 }
